80c49e040d56242880ae59fd5ab87d0e724e2589
Enrique Chua <enrique.chua29@gmail.com>
2024-06-01 17:39:19 +0800

    initial commit

--- test/flutter_ui_components_test.dart
+++ test/flutter_ui_components_test.dart
@@ -1,29 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:flutter_ui_components/flutter_ui_components.dart';
-import 'package:flutter_ui_components/flutter_ui_components_platform_interface.dart';
-import 'package:flutter_ui_components/flutter_ui_components_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockFlutterUiComponentsPlatform
-    with MockPlatformInterfaceMixin
-    implements FlutterUiComponentsPlatform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
 
 void main() {
-  final FlutterUiComponentsPlatform initialPlatform = FlutterUiComponentsPlatform.instance;
-
-  test('$MethodChannelFlutterUiComponents is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelFlutterUiComponents>());
-  });
-
-  test('getPlatformVersion', () async {
-    FlutterUiComponents flutterUiComponentsPlugin = FlutterUiComponents();
-    MockFlutterUiComponentsPlatform fakePlatform = MockFlutterUiComponentsPlatform();
-    FlutterUiComponentsPlatform.instance = fakePlatform;
-
-    expect(await flutterUiComponentsPlugin.getPlatformVersion(), '42');
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
   });
 }
